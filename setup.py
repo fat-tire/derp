c3645b2195278c214f7aefe3aba175537e56155b
fattire <f4ttire@gmail.com>
2013-05-26 23:41:34 -0700

    Use GPLv3 based on sluo/green--/hashcode's suggestion.

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 #
 # DERP- Device Environment Replacement Program
 # Copyright (C) 2013 fattire <f4ttire@gmail.com> (not the beer!)
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; version 2
+# as published by the Free Software Foundation; version 3
 # of the License.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
